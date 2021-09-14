f94ad0b0bd59ac2e52b87beadf0fcb00786b435d
ElWalto <walterclean@gmail.com>
2021-09-13 22:56:29 -0300

    correção usuario e senha atlas

--- connect/connect.js
+++ connect/connect.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const Connect = (url,user,password,banco)=>{
-    mongoose.connect(`mongodb+srv://dbadministrador:Jstq3cCIggLasGzj@todo-node.akdb3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{
+    mongoose.connect(`${url}/${banco}`,{
         user: user,
         pass: password,
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(()=>console.log('MongoDB connected'))
     .catch(err=>console.error(err));
+    
 }
 
 module.exports = Connect;
