4a97f61460196764c1e4aa7edc30418785d1a2bf
ElWalto <walterclean@gmail.com>
2021-09-14 21:41:28 -0300

    alteração connect

--- model/model.js
+++ model/model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const Model = new mongoose.Schema({
-    titulo: {type: String, required:true},
-    descricao: {type: String, required: true},
-    prioridade: {type: String, required: true},
-    estado: {type: String, required: true},
-    prazo: {type: String},
-    criacao: {type: Date, default: Date.now}
+    titulo: { type: String, required: true },
+    descricao: { type: String },
+    prioridade: { type: Number, required: true },
+    situacao: { type: String, required:true },
+    prazo: { type: Date, required: true },
+    dataCriacao: { type: Date, default: Date.now},
 })
 const toDoModel = mongoose.model('tarefas',Model);
 
